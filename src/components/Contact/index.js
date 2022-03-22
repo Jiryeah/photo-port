583a1c1076d6a307b78dd50da5e423eb8cd40713
Chidori <business.faissal@outlook.com>
2022-03-22 13:23:44 -0500

    added submit form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactForm = () => {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+
+  // This () will sync the internal state of the component formState with the user input from the DOM
+  const handleChange = (e) => {
+    // spread operator used so we can retain the other key-value pairs in this object.
+    // w/o the spread operator, the formState object would be overwritten to only contain the name: value key pair.
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
   //JSX
   return (
     <section>
       <h1>Contact me</h1>
-      <form id='contact-form'>
-        // name input
+      <form id='contact-form' onSubmit={handleSubmit}>
         <div>
           <label htmlFor='name'>Name:</label>
-          <input type='text' name='name' />
+          <input type='text' defaultValue={name} onChange={handleChange} name='name' />
         </div>
-        // email input
         <div>
           <label htmlFor='email'>Email address</label>
-          <input type='email' name='email' />
+          <input type='email' defaultValue={email} onChange={handleChange} name='email' />
         </div>
-        // message text area
         <div>
           <label htmlFor='message'>Message:</label>
-          <textarea name='message' rows='5' />
+          <textarea name='message' defaultValue={message} onChange={handleChange} rows='5' />
         </div>
         <button type='submit'>Submit</button>
       </form>
